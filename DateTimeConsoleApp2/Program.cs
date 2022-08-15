db332364e37b115a240dabd2cfd9ff881bd91e50
HASNA <hasnapvmez@gail.com>
2022-08-15 15:37:52 +0400

    first commit

---
+++ DateTimeConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDay1 = DateTime.Now;
+            
+            Console.WriteLine("Current Day and Month is : " );
+            Console.WriteLine(currentDay1.ToString("dddd, MMMM"));
+            Console.ReadKey();
+        }
+    }
+}
